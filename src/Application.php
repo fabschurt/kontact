4ca2ae408549624da8d80acc4ac8e0a6b61b3948
Fabien Schurter <fabien@fabschurt.com>
2016-11-09 23:08:05 +0100

    Simplify application initialization

--- src/Application.php
+++ src/Application.php
@@ -24,20 +24,17 @@ final class Application extends SilexApplication
      */
     public function __construct(array $values = [])
     {
-        // Define application root
+        // Initialize app
         $rootDir = __DIR__.'/..';
-
-        // Load and validate environment config
         if (is_file("{$rootDir}/.env")) {
             $dotenv = new Dotenv($rootDir);
             $dotenv->load();
         }
-
-        // Base configuration
-        $values = array_merge($values, [
+        $values = array_merge([
             'name'              => 'kontact',
             'root_dir'          => $rootDir,
             'env'               => getenv('ENVIRONMENT') ?: 'prod',
+            'debug'             => getenv('ENVIRONMENT') === 'dev',
             'admin_email'       => getenv('ADMIN_EMAIL'),
             'mailer.host'       => getenv('MAILER_HOST'),
             'mailer.port'       => getenv('MAILER_PORT'),
@@ -45,16 +42,11 @@ final class Application extends SilexApplication
             'mailer.password'   => getenv('MAILER_PASSWORD'),
             'mailer.encryption' => getenv('MAILER_ENCRYPTION'),
             'mailer.auth_mode'  => getenv('MAILER_AUTH_MODE'),
-        ]);
+        ], $values);
         parent::__construct($values);
 
-        // Activate debug mode if needed
-        $this['debug'] = ($this['env'] === 'dev');
-
-        // Helpers
+        // Register providers
         $this->register(new Provider\MailerServiceProvider());
-
-        // Controller and routing
         $this->register(new SilexProvider\ServiceControllerServiceProvider());
         $this->register(new Provider\ControllerServiceProvider());
     }
